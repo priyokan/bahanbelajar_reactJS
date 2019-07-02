780ca5514b48a3caa35f72c1db4fdf45203ddab5
Priyo Ardi Nugroho 73 <priyokan316@gmail.com>
2019-07-02 20:24:54 +0700

    adding modal delete

--- src/component/baris.js
+++ src/component/baris.js
@@ -1,15 +1,35 @@
 import React,{Component} from 'react'
-import { Button } from 'react-bootstrap';
+import { Button,ButtonToolbar, } from 'react-bootstrap';
+import Delete from './delete'
 
 class Baris extends Component{
-    render(props){
+    constructor(...args) {
+        super(...args);
+    
+        this.state = { modalShow: false };
+      } 
+    render(props){    
+          let modalClose = () => this.setState({ modalShow: false });
+       
         return(
             <tr>
                 <td>{this.props.id}</td>
                 <td>{this.props.title}</td>
                 <td>{this.props.content}</td>
-                <td>
-                    <Button variant="danger" className='m-1'>Hapus</Button>
+                <td>     
+                    <ButtonToolbar>
+                        <Button
+                        variant="primary"
+                        onClick={() => this.setState({ modalShow: true })}
+                        >
+                        Launch vertically centered modal
+                        </Button>
+
+                        <Delete
+                        show={this.state.modalShow}
+                        onHide={modalClose}
+                        />
+                    </ButtonToolbar>
                     <Button variant="info" className='m-1'>edit</Button>
                 </td>
             </tr>
